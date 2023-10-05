3ac00ef5014eff6f88a841031ea7becd7b27268f
Jeffrey Gordon <tgordon1052@gmail.com>
2023-10-05 15:43:16 -0500

    Changed .gitignore, will change back
    once done switching from Pipeline() models to API request models

--- zero_shot.py
+++ zero_shot.py
@@ -40,6 +40,19 @@ def classify(pred_labels: Dict[str, int]) -> str:
     """
     return max(pred_labels, key=pred_labels.get)
 
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/cross-encoder/nli-distilroberta-base"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+
+output = query({
+    "inputs": "Hi, I recently bought a device from your company but it is not working as advertised and I would like to get reimbursed!",
+    "parameters": {"candidate_labels": ["refund", "legal", "faq"]},
+})
 
 
 
